134b34adee43831e9e3d7fefd864b06afc5a85b2
Pablo <oliveiraapablo@gmail.com>
2024-02-22 11:35:06 -0300

    novo componente

--- js/listas-duplamente-ligadas.js
+++ js/listas-duplamente-ligadas.js
@@ -1,11 +1,13 @@
-function LinkedList() {
+function DoublyLinkedList() {
     var Node = function(element) {
         this.element = element
         this.next = null
+        this.prev = null
     }
 
     var length = 0
     var head = null
+    var tail = null
 
     this.append = function(element) {
         var node = new Node(element),
@@ -13,14 +15,15 @@ function LinkedList() {
 
         if(head === null) {
             head = node
+            tail = node
         } else {
             current = head
 
             while(current.next) {
                 current = current.next
             }
-
             current.next = node
+            tail = node
         }
         length++
     }
@@ -33,8 +36,19 @@ function LinkedList() {
             index = 0
 
             if(position === 0) {
-                node.next = current
-                head = node
+                if(!head) {
+                    head = node
+                    tail = node
+                } else {
+                    node.next = current
+                    current.prev = node
+                    head = node
+                }
+            } else if(position === length) {
+                current = tail
+                current.next = node
+                node.prev = current
+                tail = node
             } else {
                 while(index++ < position) {
                     previous = current
@@ -42,6 +56,8 @@ function LinkedList() {
                 }
                 node.next = current
                 previous.next = node
+                current.prev = node
+                node.prev = previous
             }
             length++
             return true
@@ -58,12 +74,23 @@ function LinkedList() {
 
             if(position === 0) {
                 head = current.next
+
+                if(length === 1) {
+                    tail = null
+                } else {
+                    head.prev = null
+                }
+            } else if(position === length - 1) {
+                current = tail
+                tail = current.prev
+                tail.next = null
             } else {
                 while(index++ < position) {
                     previous = current
                     current = current.next
                 }
                 previous.next = current.next
+                current.next.prev = previous
             }
             length--
             return current.element
