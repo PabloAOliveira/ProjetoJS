21cc76b02704ca424df96e5266fd9dde8d742c29
Pablo <oliveiraapablo@gmail.com>
2024-02-26 17:16:51 -0300

    Finalização dos testes e do curso

--- js/arvores.js
+++ js/arvores.js
@@ -1,42 +1,175 @@
 function BinarySearchTree() {
-    var node = function(key) {
+    var Node = function(key) {
         this.key = key
         this.left = null
         this.rigth = null
     }
     var root = null
 
-    this.insert = function(key) {
-        //insere uma chvave
+    this.insert = function(key) {      // => insere uma chave
+        var newNode = new Node(key)
+
+        if(root === null) {
+            root = newNode
+        } else {
+            insertNode(root, newNode)
+        }
+    }
+    
+    var insertNode = function(node, newNode) {
+        if(node.key > newNode.key) {
+            if(node.left === null) {
+                node.left = newNode
+            } else {
+                insertNode(node.left, newNode)
+            }
+        } else {
+            if(node.right === null) {
+                node.right = newNode
+            } else {
+                insertNode(node.right, newNode)
+            }
+        }
     }
 
-    this.search = function(key) {
-        //busca uma chave
-    } 
-
-    this.remove = function(key) {
-        //remove a chave
+    this.search = function(key) {    // => busca uma chave
+        return searchNode(root, key)
     }
 
-    this.min = function(key) {
-        //retorna a menor chave
-    }
-
-    this.max = function(key) {
-        //retorna a maior chave
-    }
-
-    this.inOrderTraverse = function(key) {
-        //visita todos os nós da arvore usando um percurso em ordem
-    }
-
-    this.preOrderTraverse = function(key) {
-        //visita todos os nós da arvore usando um percurso pré ordem
-    }
-
-    this.postOrderTraverse = function(key) {
-        //visita todos os nós da arvore usando um percurso pós ordem
+    var searchNode = function(node, key) {
+        if(node === null) {
+            return false
+        }
+        if(key < node.key) {
+            return searchNode(node.left, key)
+        } else if(key > node.key) {
+            return searchNode(node.right, key)
+        } else {
+            return true
+        }
     }
 
 
+    this.remove = function(key) {     // => remove a chave
+        root = removeNode(root, key)
+    }
+
+    var removeNode = function(node, key) {
+        if(node === null) {
+            return null
+        }
+        if(key < node.key) {
+            node.left = removeNode(node.left, key)
+            return node
+        } else if(key > node.key) {
+            node.right = removeNode(node.right, key)
+            return node
+        } else {
+            if(node.left === null && node.right === null) {
+                node = null
+                return node
+            }
+            if(node.left === null) {
+                node = node.right
+                return node
+            } else if(node.right === null) {
+                node = node.left
+                return node
+            }
+            var aux = findMinNode(node.right)
+            node.key = aux.key
+            node.right = removeNode(node.right, aux.key)
+            return node
+        }
+    }
+
+    this.min = function() {   // => retorna a menor chave
+        return minNode(root)
+    }
+
+    var minNode = function(node) {
+        if(node) {
+            while(node && node.left !== null) {
+                node = node.left
+            }
+            return node.key
+        }
+        return null
+    }
+
+    var findMinNode = function(node) {
+        while(node && node.left !== null) {
+            node = node.left
+        }
+        return node
+    }
+
+    this.max = function() {  // => retorna a maior chave
+        return maxNode(root)
+    }
+
+    var maxNode = function(node) {
+        if(node) {
+            while(node && node.right !== null) {
+                node = node.right
+            }
+            return node.key
+        }
+        return null
+    }
+
+    this.inOrderTraverse = function(callback) {  // => visita todos os nós da arvore usando um percurso em ordem
+        inOrderTraverseNode(root, callback)
+    }
+
+    var inOrderTraverseNode = function(node, callback) {
+        if(node !== null) {
+            inOrderTraverseNode(node.left, callback)
+            callback(node.key)
+            inOrderTraverseNode(node.right, callback)
+        }
+    }
+
+    this.preOrderTraverse = function(callback) {  // => visita todos os nós da arvore usando um percurso pré ordem
+        preOrderTraverseNode(root, callback)
+    }
+
+    var preOrderTraverseNode = function(node, callback) {
+        if(node !== null) {
+            callback(node.key)
+            preOrderTraverseNode(node.left, callback)
+            preOrderTraverseNode(node.right, callback)
+        }
+    }
+
+    this.postOrderTraverse = function(callback) {
+        postOrderTraverseNode(root, callback)
+    }
+
+    var postOrderTraverseNode = function(node, callback) {  // => visita todos os nós da arvore usando um percurso pós ordem
+        if(node !== null) {
+            postOrderTraverseNode(node.left, callback)
+            postOrderTraverseNode(node.right, callback)
+            callback(node.key)
+        }
+    }
 }
+
+var tree = new BinarySearchTree()
+tree.insert(11)
+tree.insert(7)
+tree.insert(15)
+tree.insert(5)
+tree.insert(3)
+tree.insert(9)
+tree.insert(8)
+tree.insert(10)
+tree.insert(13)
+tree.insert(12)
+tree.insert(14)
+tree.insert(20)
+tree.insert(18)
+tree.insert(25)
+tree.insert(6)
+
+console.log(tree.min)
