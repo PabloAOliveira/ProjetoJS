bbace1371c43be8670b1be4c447342445fe74cd1
Pablo <oliveiraapablo@gmail.com>
2024-02-26 10:29:02 -0300

    HashTable

---
+++ js/dicionarios-e-hashes.js
@@ -0,0 +1,111 @@
+function Dictionary() {
+    var items = {}
+
+    this.set = function(key, value) {
+        items[key] = value
+    }
+
+    this.delete = function(key) {
+        if(this.has(key)) {
+            delete items[key]
+            return true
+        }
+        return false
+    }
+
+    this.has = function(key) {
+        return items.hasOwnProperty(key)
+    }
+
+    this.get = function(key) {
+        return this.has(key) ? items[key] : undefined
+    }
+
+    this.clear = function() {
+        items = {}
+    }
+
+    this.size = function() {
+        return Object.keys(items).length
+    }
+
+    this.keys = function() {
+        return Object.keys(items)
+    }
+
+    this.values = function() {
+        var values = [],
+        keys = Object.keys(items)
+
+        for(var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]])
+        }
+        return values
+    }
+
+    this.getItems = function() {
+        return items
+    }
+}
+
+function HashTable() {
+    var table = []
+
+    var ValuePair = function(key, value) {
+        this.key = key
+        this.value = value
+        this.toString = function() {
+            return '[' + this.key + ' - ' + this.value + ']'
+        }
+    }
+
+    this.put = function(key, value) {
+        var position = loseloseHashCode(key)
+        console.log(position + ' ' + key)
+        table[position] = value
+    }
+
+    this.remove = function(key) {
+        table[loseloseHashCode(key)] = undefined
+    }
+
+    this.get = function(key) {
+        var position = loseloseHashCode(key)
+
+        if(table[position] !== undefined) {
+            var current = table[position].getHead()
+
+            while(current.next) {
+                if(current.element.key == key) {
+                    return current.element.value
+                }
+                current = current.next
+            }
+            if(current.element.key === key) {
+                return current.element.value
+            }
+        }
+        return undefined
+    }
+
+    var loseloseHashCode = function(key) {
+        var hash = 0
+        for(var i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i)
+        }
+        return hash % 37
+    }
+
+    this.print = function() {
+        for(var i = 0; i < table.length; i++) {
+            if(table[i] !== undefined) {
+                console.log(i + ' : ' + table[i])
+            }
+        }
+    }
+
+    this.size = function() {
+        return table.length
+    }
+}
+
