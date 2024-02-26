9c95c4b39e51778563c4fece9d4f529baa3e7380
Pablo <oliveiraapablo@gmail.com>
2024-02-26 10:52:00 -0300

    Adicionando Arvores

---
+++ js/arvores.js
@@ -0,0 +1,42 @@
+function BinarySearchTree() {
+    var node = function(key) {
+        this.key = key
+        this.left = null
+        this.rigth = null
+    }
+    var root = null
+
+    this.insert = function(key) {
+        //insere uma chvave
+    }
+
+    this.search = function(key) {
+        //busca uma chave
+    } 
+
+    this.remove = function(key) {
+        //remove a chave
+    }
+
+    this.min = function(key) {
+        //retorna a menor chave
+    }
+
+    this.max = function(key) {
+        //retorna a maior chave
+    }
+
+    this.inOrderTraverse = function(key) {
+        //visita todos os nós da arvore usando um percurso em ordem
+    }
+
+    this.preOrderTraverse = function(key) {
+        //visita todos os nós da arvore usando um percurso pré ordem
+    }
+
+    this.postOrderTraverse = function(key) {
+        //visita todos os nós da arvore usando um percurso pós ordem
+    }
+
+
+}
